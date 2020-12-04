c14c5245e0b3027cfeccae33b9b8754833049b0a
Fabian Mueller7 <Fabian_Mueller7@sluz.ch>
2020-12-04 11:26:38 +0100

    End Lection 1

---
+++ M411Project/Main.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Elm {
+    int Nr;
+    struct Elm* pNext; 
+} struElm;
+
+for (intiElm = 0; iElm < Anzahl; iElm++) {
+    // Element erstellen und initialisieren
+    pNew = (struElm*)malloc(sizeof(struElm));
