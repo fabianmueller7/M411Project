4b063293b8c4c854f58bfe2fac20eec7d0e33502
Lian Aeschlimann <lian.aeschli@gmail.com>
2020-12-18 11:23:27 +0100

    Grösse wird abgefragt, nicht mögliche auswahlen werden angezeigt, Sortierung abfrage

--- M411Project/DarstelungListe.cpp
+++ M411Project/DarstelungListe.cpp
@@ -1,7 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-char auswahl;
+
+
+int auswahl;
+int groesse;
+bool listeExestiert = false;
+int Liste[1000000];
+char elemente;
+char upDown;
 
 void load_menu();
 
@@ -13,29 +20,64 @@ int main()
 
 void auswahl_treffen()
 {
-	auswahl = getchar();
+	scanf_s("%i", &auswahl, 1);
 	switch (auswahl) {
-	case '1':
+	case 1:
 		system("cls");
-		printf("grösse\n");
+		printf("Wie gross soll die Liste sein?\n");
+		scanf_s("%i", &groesse);
+		for (int i = 0; i < groesse; i++)
+			Liste[i] = rand();
+		listeExestiert = true;
 		break;
 
-	case '2':
-		system("cls");
-		printf("gelöscht\n");
+	case 2:
+		if (listeExestiert)
+		{
+			system("cls");
+			listeExestiert = false;
+			printf("gelöscht\n");
+
+		}
+		else
+		{
+			system("cls");
+			printf("Invalide auswahl\n");
+		}
+		break;
+	case 3:
+		if (listeExestiert)
+		{
+			system("cls");
+			printf("Element zum Sortieren(Bezeichnung(b)/Preis(p)):\t");
+			//Wird noch nicht richtig abgefragt
+			scanf_s("%c", &elemente, 1);
+			printf("\nRichtung(Steigend(u)/Sinkend(d)):\t");
+			scanf_s("%c", &upDown, 1);
+
+		}
+		else
+		{
+			system("cls");
+			printf("Invalide auswahl\n");
+		}
 		break;
 
-	case '3':
-		system("cls");
-		printf("Elemente: \nRichtung:");
+	case 4:
+		if (listeExestiert)
+		{
+			system("cls");
+			printf("<Liste>\n");
+		}
+		else
+		{
+			system("cls");
+			printf("Invalide auswahl\n");
+		}
+
 		break;
 
-	case '4':
-		system("cls");
-		printf("<Liste>\n");
-		break;
-
-	case '5':
+	case 5:
 		system("pause");
 		return;
 	default:
@@ -45,9 +87,8 @@ void auswahl_treffen()
 			printf("Falsche auswahl\nDu hast eingegeben: %c ", auswahl);
 			system("pause");
 		}
-
 	}
-	system("pause");
+	system("pause");	
 	load_menu();
 }
 
@@ -55,13 +96,17 @@ void load_menu()
 {
 	system("cls");
 	printf_s("1. Liste erstellen \n");
-	if (false/*liste exestiert*/)
+	if (listeExestiert)
 	{
-		//Farbe grau
+		printf("2. Liste löschen \n");
+		printf("3. Liste Sortieren\n");
+		printf("4. Liste ausgeben\n");
+	}else
+	{
+		printf("\x1B[31m2. Liste loeschen\033[0m\n");
+		printf("\x1B[31m3. Liste Sortieren\033[0m\n");
+		printf("\x1B[31m4. Liste ausgeben\033[0m\n");
 	}
-	printf("2. Liste löschen \n");
-	printf("3. Liste Sortieren\n");
-	printf("4. Liste ausgeben\n");
 	printf("5. Programm beenden\n");
 	auswahl_treffen();
 }
